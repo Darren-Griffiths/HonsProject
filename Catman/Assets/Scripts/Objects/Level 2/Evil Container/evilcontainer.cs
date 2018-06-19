b8e478a19cf5bd43667e55fe79c4c1f06aab4c95
Darren <darrengriffiths316@outlook.com>
2018-06-19 13:47:22 +0100

    Evil Container
    Container moves until certain x value is met.
    
    Had to apply speed, remove static and apply a condition for the position to meet.

--- Catman/Assets/Scripts/Objects/Level 2/Evil Container/evilcontainer.cs
+++ Catman/Assets/Scripts/Objects/Level 2/Evil Container/evilcontainer.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class evilcontainer : MonoBehaviour {
 
+    public float speed = 1;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+
+      
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+
+        if (this.gameObject.transform.position.z >= -24.50)
+        {
+            this.gameObject.transform.position += Vector3.back * speed * Time.deltaTime;
+        }
 		
 	}
 }
