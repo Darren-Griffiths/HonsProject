5033449e56c09f588010ab0f0cc95a311c79d125
Darren <darrengriffiths316@outlook.com>
2019-03-06 16:37:22 +0000

    Level 1 button added and object now appears at start of game with font scaling fix

--- Catman/Assets/Scripts/UI/Level 1/Inventory/Inventory.cs
+++ Catman/Assets/Scripts/UI/Level 1/Inventory/Inventory.cs
@@ -16,19 +16,20 @@ public class Inventory : MonoBehaviour {
     private UnityStandardAssets.Characters.FirstPerson.MouseLook mouselookScript;
     public GameObject MouseLook;
 
+    public GameObject startGame;
+
     // Use this for initialization
     void Start ()
     {
         startmenuScript = StartMenu.GetComponent<StartMenu>();
         mouselookScript = MouseLook.GetComponent<UnityStandardAssets.Characters.FirstPerson.MouseLook>();
-        show = false;
         ESC = false;
-        transform.localPosition = new Vector3(0, 9000, 0);
         //print(transform.localPosition.y);
         //print(show);
 
         ///Inventory paper text
         SetPaperText();
+        startTrigger();
     }
 
     // Update is called once per frame
@@ -75,5 +76,21 @@ public class Inventory : MonoBehaviour {
 
         paperText.text = PlayerPrefs.GetString("characterName") + ",\n\nI have been watching you. \nfor so long I have been wanting you. \nNow that I have you.\nI feel so guilty for taken you." +
         "\nIt pains me to think you don't want me.\n\nSo heres the deal, \n\nI will give you NINE chances to show me that you want me too...\nminus the one chance I have just giving you.";
+    }
+
+    void startTrigger()
+    {
+        show = true;
+        transform.localPosition = new Vector3(0, 0, 0);
+        mouselookScript.m_cursorIsLocked = false;
+    }
+
+    public void startButton()
+    {
+        show = false;
+        transform.localPosition = new Vector3(0, 0, 9000);
+        mouselookScript.m_cursorIsLocked = true;
+        startGame.SetActive(false); 
+
     }
 }
