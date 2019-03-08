07a0e7253d784d0d91d12cdcb87b53b62d74e85c
Darren <darrengriffiths316@outlook.com>
2019-03-08 15:50:16 +0000

    new audio scripting code

--- Catman/Assets/Scripts/Objects/Level 2/Evil Container/evilcontainer.cs
+++ Catman/Assets/Scripts/Objects/Level 2/Evil Container/evilcontainer.cs
@@ -6,19 +6,24 @@ public class evilcontainer : MonoBehaviour {
 
     public float speed = 1;
     public bool moving;
+    public bool introPlaying;
     public AudioSource engine;
     public AudioSource enginedown;
+    public AudioSource metalIntro;
+    public AudioSource metalLoop;
+    public AudioSource metalOutro;
+    public AudioClip outro;
 
     // Use this for initialization
     void Start ()
     {
-        
+        Invoke("metalStart", 0.5f);
+        Invoke("metalloopStart", 3);
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-
+  
         if (this.gameObject.transform.position.z >= -24.50)
         {
             this.gameObject.transform.position += Vector3.back * speed * Time.deltaTime;
@@ -31,13 +36,22 @@ public class evilcontainer : MonoBehaviour {
         if (moving == false)
         {
             engine.Stop();
-            
+            metalLoop.Stop();
         }
-		
 	}
 
-    void playDown()
+    void metalStart()
     {
-        
+        metalIntro.Play();
     }
+
+    void metalloopStart()
+    {
+        metalLoop.Play();
+    }
+    void metaloutroStart()
+    {
+        metalOutro.Play();
+    }
+
 }
